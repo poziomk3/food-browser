5389a22289aebf80883b0ba459325daa0a2c7b89
Karol Zając <karolzajac.0407@gmail.com>
2023-12-22 22:06:03 +0100

    chore: es lint add, navbar styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,6 @@ import { MaterialModule } from './shared/material/material.service';
   standalone: true,
   imports: [CommonModule, RouterOutlet,MaterialModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
 })
 export class AppComponent {
 }
--- src/app/food/features/food-main/food-main.component.ts
+++ src/app/food/features/food-main/food-main.component.ts
@@ -1,14 +1,25 @@
-import { Component } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { Component, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
 import { MaterialModule } from '../../../shared/material/material.service';
 
 @Component({
   selector: 'app-food-main',
   standalone: true,
-  imports: [RouterModule,MaterialModule],
+  imports: [RouterModule, MaterialModule],
   templateUrl: './food-main.component.html',
-  styleUrl: './food-main.component.scss'
+  styleUrl: './food-main.component.scss',
 })
-export class FoodMainComponent {
+export class FoodMainComponent implements OnChanges, OnInit {
+  activePage?: string | null;
+  constructor(private router: Router) {}
 
+  activateRoute() {
+    this.activePage = this.router.url.split('/')[2];
+  }
+  ngOnInit(): void {
+    this.activateRoute();
+  }
+  ngOnChanges(changes: SimpleChanges): void {
+    this.activateRoute();
+  }
 }
