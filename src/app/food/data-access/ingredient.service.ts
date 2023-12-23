b5045d3d4df557acae47a5cc9926faef11d846ac
Karol Zając <karolzajac.0407@gmail.com>
2023-12-23 15:31:55 +0100

    feat:  ingredient service with pagination

--- src/app/food/data-access/ingredient.service.ts
+++ src/app/food/data-access/ingredient.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, map } from 'rxjs';
+import { BehaviorSubject, Observable, combineLatest, of } from 'rxjs';
+import { map, switchMap, tap } from 'rxjs/operators';
 export interface Product {
   idIngredient: number;
   strIngredient: string;
@@ -16,11 +17,46 @@ export interface IngredientsDTO {
 })
 export class IngredientService {
   constructor(private http: HttpClient) {}
+  ingredientsPersisted$: BehaviorSubject<Array<Product>> = new BehaviorSubject<
+    Array<Product>
+  >([]);
 
-  getAllIngredients(): Observable<Array<Product>> {
+  itemsPerPages$: BehaviorSubject<number> = new BehaviorSubject<number>(1);
+  numberOfPages$: BehaviorSubject<number> = new BehaviorSubject<number>(1);
+
+  getAllIngredients(): void {
     const url = 'https://www.themealdb.com/api/json/v1/1/list.php?i=list';
-    return this.http
+    this.http
       .get<IngredientsDTO>(url)
-      .pipe(map((data) => data.meals.slice(0, 60)));
+      .pipe(
+        map((data) => data.meals),
+        tap((ingredients) => {
+          this.ingredientsPersisted$.next(ingredients);
+          this.numberOfPages$.next(
+            Math.ceil(ingredients.length / this.itemsPerPages$.value)
+          );
+        })
+      )
+      .subscribe();
+  }
+
+  getPageOfIngredients(page$: Observable<number>): Observable<Array<Product>> {
+    if (this.ingredientsPersisted$.value.length === 0) this.getAllIngredients();
+    return combineLatest([this.ingredientsPersisted$, page$]).pipe(
+      switchMap(([ingredients, page]) => {
+        const startIndex = (page - 1) * this.itemsPerPages$.value;
+        const endIndex = startIndex + this.itemsPerPages$.value;
+        return of(ingredients.slice(startIndex, endIndex));
+      })
+    );
+  }
+
+  setNumberOfIngredientsOnPage(numberOfIngs: number): void {
+    this.itemsPerPages$.next(numberOfIngs);
+    this.numberOfPages$.next(
+      Math.ceil(
+        this.ingredientsPersisted$.value.length / this.itemsPerPages$.value
+      )
+    );
   }
 }
--- src/app/food/features/ingredients-list/ingredients-list.component.ts
+++ src/app/food/features/ingredients-list/ingredients-list.component.ts
@@ -1,36 +1,36 @@
-import { Component, OnInit } from '@angular/core';
-import { Observable, map, tap } from 'rxjs';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
 import {
   IngredientService,
   Product,
 } from '../../data-access/ingredient.service';
-import { AsyncPipe, CommonModule, JsonPipe } from '@angular/common';
+import { AsyncPipe, JsonPipe } from '@angular/common';
 import { MaterialModule } from '../../../shared/material/material.service';
-
 @Component({
   selector: 'app-ingredients-list',
   standalone: true,
-  imports: [AsyncPipe, JsonPipe,MaterialModule],
+  imports: [AsyncPipe, JsonPipe, MaterialModule],
   templateUrl: './ingredients-list.component.html',
   styleUrl: './ingredients-list.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class IngredientsListComponent implements OnInit {
   allIngredients: Observable<Array<Product>> | null = null;
-  pictures: { [key: number]: Observable<string> } = {};
+  currentPage$: BehaviorSubject<number> = new BehaviorSubject<number>(1);
+  numberOfPages$: BehaviorSubject<number> = new BehaviorSubject<number>(1);
+
   constructor(private ingredientService: IngredientService) {}
   ngOnInit(): void {
-    this.allIngredients = this.ingredientService
-      .getAllIngredients()
-      .pipe
-      // tap((data) =>
-      //   data.map(
-      //     (ingredient) =>
-      //       (this.pictures[ingredient.idIngredient] =
-      //         this.ingredientService.getIngredientPicture(
-      //           ingredient.strIngredient
-      //         ))
-      //   )
-      // )
-      ();
+    this.allIngredients = this.ingredientService.getPageOfIngredients(
+      this.currentPage$
+    );
+    this.numberOfPages$ = this.ingredientService.numberOfPages$;
+  }
+
+  updateCurrentPage(newPage: number): void {
+    this.currentPage$.next(newPage);
+  }
+  nextPage() {
+    this.currentPage$.next(this.currentPage$.value + 1);
   }
 }
