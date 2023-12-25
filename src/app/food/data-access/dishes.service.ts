3eae7247e79e65cac39e0e8f3090df388b1e1d9a
Karol Zając <karolzajac.0407@gmail.com>
2023-12-25 22:42:25 +0100

    style: filter ui

--- src/app/food/data-access/dishes.service.ts
+++ src/app/food/data-access/dishes.service.ts
@@ -3,23 +3,32 @@ import { DatapaginationService } from './datapagination.service';
 import { HttpClient } from '@angular/common/http';
 import { BehaviorSubject, Observable, map, switchMap, tap } from 'rxjs';
 import {
+  Area,
+  Category,
   MealDetailsDTO,
   MealNoDetails,
   MealWithDetails,
   MealsDTO,
 } from '../models/Dish';
 
+export interface FilterInterface{
+  strCategory?: string[]|null;
+  strArea?: string[]|null;
+}
 @Injectable({
   providedIn: 'root',
 })
-export class DishesService {
+export class DishesService  {
   paginator: DatapaginationService = new DatapaginationService();
   http = inject(HttpClient);
 
   persistedData$: BehaviorSubject<Array<MealNoDetails>> = new BehaviorSubject<
     Array<MealNoDetails>
   >([]);
-
+  categories$: BehaviorSubject<Array<Category>> = new BehaviorSubject<
+    Array<Category>
+  >([]);
+  areas$: BehaviorSubject<Array<Area>> = new BehaviorSubject<Array<Area>>([]);
   // https://www.themealdb.com/api/json/v1/1/search.php?s= albo https://www.themealdb.com/api/json/v1/1/filter.php?i=
   fetchAPI(): void {
     const url = 'https://www.themealdb.com/api/json/v1/1/filter.php?i=';
@@ -33,6 +42,34 @@ export class DishesService {
       )
       .subscribe();
   }
+
+  fetchCategories(): void {
+    const url = 'https://www.themealdb.com/api/json/v1/1/list.php?c=list';
+    this.http
+      .get<{ meals: Array<Category> }>(url)
+      .pipe(map(data => data.meals))
+      .subscribe(categories => this.categories$.next(categories));
+  }
+
+  fetchAreas(): void {
+    const url = 'https://www.themealdb.com/api/json/v1/1/list.php?a=list';
+    this.http
+      .get<{ meals: Array<Area> }>(url)
+      .pipe(map(data => data.meals))
+      .subscribe(areas => this.areas$.next(areas));
+  }
+
+  getDishCategories(): Observable<Array<Category>> {
+    if (this.categories$.value.length === 0) this.fetchCategories();
+    return this.categories$;
+  }
+  getDishAreas(): Observable<Array<Area>>{
+    if (this.areas$.value.length === 0) this.fetchAreas();
+    return this.areas$;
+  }
+
+  
+
 
   getDishDetails(id: string): Observable<MealWithDetails> {
     const url = 'https://www.themealdb.com/api/json/v1/1/lookup.php?i=' + id;
--- src/app/food/features/dishes-list/dishes-list.component.ts
+++ src/app/food/features/dishes-list/dishes-list.component.ts
@@ -1,10 +1,5 @@
-import {
-  ChangeDetectionStrategy,
-  Component,
-  OnInit,
-  inject,
-} from '@angular/core';
-import { BehaviorSubject, Observable } from 'rxjs';
+import { Component, OnInit, inject } from '@angular/core';
+import { BehaviorSubject, Observable, combineLatest, map, tap } from 'rxjs';
 import { DishesService } from '../../data-access/dishes.service';
 import { PageEvent } from '@angular/material/paginator';
 import { MaterialModule } from '../../../shared/material/material.service';
@@ -12,13 +7,20 @@ import { AsyncPipe } from '@angular/common';
 import { FoodCardComponent } from '../../ui/food-card/food-card.component';
 import { RouterModule } from '@angular/router';
 import { PageInUrlComponent } from '../page-in-url/page-in-url.component';
-import { MealNoDetails } from '../../models/Dish';
+import { Area, Category, MealNoDetails } from '../../models/Dish';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-dishes-list',
   standalone: true,
-  imports: [AsyncPipe, MaterialModule, FoodCardComponent, RouterModule],
-  changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [
+    AsyncPipe,
+    MaterialModule,
+    FoodCardComponent,
+    ReactiveFormsModule,
+    RouterModule,
+  ],
+
   templateUrl: './dishes-list.component.html',
   styleUrl: './dishes-list.component.scss',
 })
@@ -31,14 +33,32 @@ export class DishesListComponent extends PageInUrlComponent implements OnInit {
   itemsOnPage$: BehaviorSubject<number> = new BehaviorSubject<number>(0);
   allIngredientsLength$: Observable<number> | null = null;
 
+  categories$: Observable<Array<Category>> | null = null;
+  areas$: Observable<Array<Area>> | null = null;
+
+  activeCategories = new FormControl<Array<string>>([]);
+  activeAreas = new FormControl<Array<string>>([]);
+
   override ngOnInit(): void {
     this.addQueryParam('page', '0');
     this.addQueryParam('pageSize', '5');
     super.ngOnInit();
 
     this.itemsOnPage$ = this.dishesService.getNumberOnPage();
-    this.allDishes$ = this.dishesService.getPage(this.currentPage$);
+    this.allDishes$ = this.dishesService
+      .getPage(this.currentPage$)
+      .pipe(tap(data => console.log(data)));
     this.allIngredientsLength$ = this.dishesService.getFullLength();
+    this.areas$ = this.dishesService.getDishAreas();
+    this.categories$ = this.dishesService.getDishCategories();
+
+    this.activeCategories.valueChanges.subscribe(val => {
+      console.log(val);
+    });
+    ``;
+    this.activeAreas.valueChanges.subscribe(val => {
+      console.log(val);
+    });
 
     if (this.paramsMap.get('pageSize')?.value > 0) {
       const val = 1 * this.paramsMap.get('pageSize')?.value;
@@ -62,5 +82,17 @@ export class DishesListComponent extends PageInUrlComponent implements OnInit {
     this.router.navigate(['food', 'dishes', 'details', arg], {
       queryParams: { page: 0 },
     });
+  }
+
+  removeCat(_t3: string) {
+    if (this.activeCategories)
+      this.activeCategories.setValue(
+        this.activeCategories.value!.filter(t => t !== _t3)
+      );
+  }
+
+  removeArea(_t3: string) {
+    if (this.activeAreas)
+      this.activeAreas.setValue(this.activeAreas.value!.filter(t => t !== _t3));
   }
 }
